06921b1df7f84f8289acbcc1505b198458e9e4c5
Natalie Marleny <nataliemarleny@gmail.com>
2018-07-30 06:55:43 +0100

    Add heatmap files
    - Forked from DKirwan/calendar-heatmap

---
+++ js/index.js
@@ -0,0 +1,30 @@
+var now = moment().endOf('day').toDate();
+var yearAgo = moment().startOf('day').subtract(1, 'year').toDate();
+
+var customLocale = {
+months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', '', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+days: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+No: 'No',
+on: 'on',
+Less: 'Less',
+More: 'More'
+}
+
+d3.json("data.json").then(function (chartData) {
+// Convert all date strings to the date-objects
+for (var i = 0; i < chartData.length; i += 1) {
+    chartData[i].date = new Date(chartData[i].date)
+}
+
+// Customize the heatmap
+var heatmap = calendarHeatmap()
+    .locale(customLocale)
+    .data(chartData)
+    .selector('.container')
+    .tooltipEnabled(false)
+    .colorRange(['#f4f7f7', '#79a8a9'])
+    .onClick(function (data) {
+    console.log('data', data);
+    });
+heatmap();  // render the chart
+});
